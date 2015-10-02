b6ceb4d1b65a4797afcacf3b7d948139ff95e050
Andres Arrigonni <AArrigonni@infragistics.com>
2015-10-02 10:00:22 -0300

    fotos

--- Hackweak/www/js/Controllers/roomsController.js
+++ Hackweak/www/js/Controllers/roomsController.js
@@ -1,8 +1,54 @@
 ﻿module.controller('RoomsController', function ($scope, $data, $http) {
 
-   
+$scope.init = function() {
+
+var delegate = new cordova.plugins.locationManager.Delegate();
+
+       delegate.didDetermineStateForRegion = function (pluginResult) {
+           $scope.text = '[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult);
+       };
+
+       delegate.didStartMonitoringForRegion = function (pluginResult) {
+           $scope.text = 'didStartMonitoringForRegion: ' + pluginResult;
+       };
+
+       delegate.didRangeBeaconsInRegion = function (pluginResult) {
+
+           $scope.text = '[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult);
+       };
+
+       var region = createBeacon();
+
+       cordova.plugins.locationManager.setDelegate(delegate);
+
+       // required in iOS 8+
+       cordova.plugins.locationManager.requestWhenInUseAuthorization();
+       // or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+       cordova.plugins.locationManager.startMonitoringForRegion(region)
+           .fail(console.error)
+           .done();
+    
+}
+
+    $scope.init();
+
+
 });
 
+
+function createBeacon() {
+
+    var uuid = 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825'; // mandatory
+    var identifier = 'IG MVD'; // mandatory
+    //  var minor = 1000; // optional, defaults to wildcard if left empty
+    var major = 10004; // optional, defaults to wildcard if left empty
+
+    // throws an error if the parameters are not valid
+    var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major);
+
+    return beaconRegion;
+}
 
 /*$scope.items = $data.items;
 
@@ -36,17 +82,5 @@
       };*/
 
 
-/*
-function createBeacon() {
-
-    var uuid = 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825'; // mandatory
-    var identifier = 'IG MVD'; // mandatory
-  //  var minor = 1000; // optional, defaults to wildcard if left empty
-    var major = 10004; // optional, defaults to wildcard if left empty
-
-    // throws an error if the parameters are not valid
-    var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major);
-
-    return beaconRegion;
-}*/
+/**/
 
